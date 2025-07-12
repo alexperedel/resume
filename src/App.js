3a2ad44cab727bd00e4c324f9e9c007b3bf960f4
Aleksandr <20129072@tafe.wa.edu.au>
2025-07-13 03:38:26 +0800

    feat(add-page): Add Optus page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Telstra from './pages/Telstra';
+import Optus from "./pages/Optus";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/telstra" element={<Telstra />} />
+                <Route path="/optus-letter" element={<Optus />} />
                 <Route path="*" element={<h1>404: Page Not Found</h1>} />
             </Routes>
         </Router>
