bab10d0b0f05dd9df08e66e7564c6b2261692c9a
Aleksandr <20129072@tafe.wa.edu.au>
2025-08-13 11:50:04 +0800

    fix(headers): quick fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,9 @@ const Header = () => {
         <div className="bg-gray-50 flex pt-1 flex-col items-center relative">
             <header className="text-center sm:text-left max-w-3xl w-full px-6">
                 <h1 className="text-4xl font-extrabold text-green-900">Alex Peredelskii</h1>
-                <p className="text-gray-700 mt-2 text-lg">Creating better customer experiences</p>
             </header>
 
-            {/* Dark slate gray separator */}
-            <div className="w-full max-w-3xl h-1 my-6 bg-gray-700 rounded-full px-3" />
+            <div className="w-2/3 max-w-3xl h-1 my-6 bg-gray-700 rounded-full" />
         </div>
     );
 };
